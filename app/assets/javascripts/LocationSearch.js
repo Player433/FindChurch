189c305527fa3375c09af0577b2ebdba9f0e9164
Gary Boicourt <player433@gmail.com>
2014-03-17 20:40:16 +0000

    Adding client side geocoding javascript.

---
+++ app/assets/javascripts/LocationSearch.js
@@ -0,0 +1,20 @@
+
+function geocode() {
+  var geocoder = new google.maps.Geocoder();
+  $(search).bind("ajax:before", function(){
+    if (coordinatesField.val() == "" && geocoder){
+      geocoder.geocode({'address':search.val()}, (results, status) =>
+        if (status == google.maps.GeocoderStatus.OK){
+          coordinates.val(results[0].geometry.location);
+          alert "coordinates: #{coordinatesField.val()}";
+          searchForm.trigger("submit.rails");
+        };
+      );
+        
+      return false;
+    };
+    else{
+      return true;
+    }
+  });
+};
